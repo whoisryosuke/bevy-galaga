2068794a7ab8f5d94c6efb898a924448447d6787
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2023-02-03 13:13:56 -0800

    :sparkles: projectile ecs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
 
 fn main() {
     App::new()
@@ -7,7 +7,67 @@ fn main() {
         .run();
 }
 
-fn setup_game(mut commands: Commands) {
+// The Player object
+#[derive(Component)]
+struct Player;
+
+// The projectile spawned by Player firing weapon
+#[derive(Component)]
+struct Projectile;
+
+// The speed of an object
+#[derive(Component, Deref, DerefMut)]
+struct Velocity(Vec2);
+
+// Signifies an object is collidable
+#[derive(Component)]
+struct Collider;
+
+const PADDLE_SIZE: Vec3 = Vec3::new(120.0, 20.0, 0.0);
+const BALL_STARTING_POSITION: Vec3 = Vec3::new(0.0, 20.0, 0.0);
+const BALL_SIZE: Vec3 = Vec3::new(10.0, 10.0, 0.0);
+const BALL_SPEED: f32 = 400.0;
+const INITIAL_BALL_DIRECTION: Vec2 = Vec2::new(0.5, -0.5);
+
+const PADDLE_COLOR: Color = Color::rgb(0.3, 0.3, 0.7);
+const BALL_COLOR: Color = Color::rgb(0.7, 0.87, 0.7);
+
+fn setup_game(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    asset_server: Res<AssetServer>,
+) {
     // Camera
     commands.spawn(Camera2dBundle::default());
+
+    // Spawn Player in initial position
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(0.0, 0.0, 0.0),
+                scale: PADDLE_SIZE,
+                ..default()
+            },
+            sprite: Sprite {
+                color: PADDLE_COLOR,
+                ..default()
+            },
+            ..default()
+        },
+        Player,
+        Collider,
+    ));
+
+    // Ball
+    commands.spawn((
+        MaterialMesh2dBundle {
+            mesh: meshes.add(shape::Circle::default().into()).into(),
+            material: materials.add(ColorMaterial::from(BALL_COLOR)),
+            transform: Transform::from_translation(BALL_STARTING_POSITION).with_scale(BALL_SIZE),
+            ..default()
+        },
+        Projectile,
+        Velocity(INITIAL_BALL_DIRECTION.normalize() * BALL_SPEED),
+    ));
 }
