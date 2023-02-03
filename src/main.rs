1998efe9a8980d3f3fc5cc17fac9199620cd17e6
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2023-02-03 15:33:14 -0800

    :zap: rename vars

--- src/main.rs
+++ src/main.rs
@@ -33,15 +33,15 @@ struct Collider;
 // in this case, 60fps
 const TIME_STEP: f32 = 1.0 / 60.0;
 
-const PADDLE_SIZE: Vec3 = Vec3::new(120.0, 20.0, 0.0);
+const PLAYER_SIZE: Vec3 = Vec3::new(120.0, 20.0, 0.0);
 const PLAYER_SPEED: f32 = 100.0;
-const BALL_STARTING_POSITION: Vec3 = Vec3::new(0.0, 20.0, 0.0);
-const BALL_SIZE: Vec3 = Vec3::new(10.0, 10.0, 0.0);
-const BALL_SPEED: f32 = 400.0;
-const INITIAL_BALL_DIRECTION: Vec2 = Vec2::new(0.5, -0.5);
+const PROJECTILE_STARTING_POSITION: Vec3 = Vec3::new(0.0, 20.0, 0.0);
+const PROJECTILE_SIZE: Vec3 = Vec3::new(10.0, 10.0, 0.0);
+const PROJECTILE_SPEED: f32 = 400.0;
+const INITIAL_PROJECTILE_DIRECTION: Vec2 = Vec2::new(0.5, -0.5);
 
-const PADDLE_COLOR: Color = Color::rgb(0.3, 0.3, 0.7);
-const BALL_COLOR: Color = Color::rgb(0.7, 0.87, 0.7);
+const PLAYER_COLOR: Color = Color::rgb(0.3, 0.3, 0.7);
+const PROJECTILE_COLOR: Color = Color::rgb(0.7, 0.87, 0.7);
 
 fn setup_game(
     mut commands: Commands,
@@ -56,12 +56,12 @@ fn setup_game(
     commands.spawn((
         SpriteBundle {
             transform: Transform {
-                translation: Vec3::new(0.0, 0.0, 0.0),
-                scale: PADDLE_SIZE,
+                translation: Vec3::new(0.0, -250.0, 0.0),
+                scale: PLAYER_SIZE,
                 ..default()
             },
             sprite: Sprite {
-                color: PADDLE_COLOR,
+                color: PLAYER_COLOR,
                 ..default()
             },
             ..default()
@@ -74,12 +74,13 @@ fn setup_game(
     commands.spawn((
         MaterialMesh2dBundle {
             mesh: meshes.add(shape::Circle::default().into()).into(),
-            material: materials.add(ColorMaterial::from(BALL_COLOR)),
-            transform: Transform::from_translation(BALL_STARTING_POSITION).with_scale(BALL_SIZE),
+            material: materials.add(ColorMaterial::from(PROJECTILE_COLOR)),
+            transform: Transform::from_translation(PROJECTILE_STARTING_POSITION)
+                .with_scale(PROJECTILE_SIZE),
             ..default()
         },
         Projectile,
-        Velocity(INITIAL_BALL_DIRECTION.normalize() * BALL_SPEED),
+        Velocity(INITIAL_PROJECTILE_DIRECTION.normalize() * PROJECTILE_SPEED),
     ));
 }
 
@@ -91,17 +92,16 @@ fn move_player(
     let mut direction = 0.0;
 
     if keyboard_input.pressed(KeyCode::Left) {
-        println!("[KEYBOARD] Pressed left");
         direction -= 1.0;
     }
 
     if keyboard_input.pressed(KeyCode::Right) {
-        println!("[KEYBOARD] Pressed right");
         direction += 1.0;
     }
 
     // Calculate the new horizontal paddle position based on player input
     let new_paddle_position = paddle_transform.translation.x + direction * PLAYER_SPEED * TIME_STEP;
+    // TODO: make sure player doesn't exceed bounds of game area
 
     paddle_transform.translation.x = new_paddle_position;
 }
